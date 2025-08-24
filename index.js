6c32cd329f9cd0bea75e26ab136e1fcde62f2ffd
Darshan Karagir <karagirdarshan1@gmail.com>
2025-08-24 16:17:03 +0530

    some bugs

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
- let currentPage = 0;
-    const pages = $(".page");
+let currentPage = 0;
+const pages = $(".page");
 
-    function showPage(index) {
-      pages.hide();
-      $(pages[index]).css("display", "flex");
-    }
+function showPage(index) {
+  pages.hide();
+  $(pages[index]).css("display", "flex");
+}
 
-    $(".prev-btn").click(function(){
-      if (currentPage > 0) {
-        currentPage--;
-        showPage(currentPage);
-      }
-    })
-
-    $(".next-btn").click(function(){
-      if (currentPage < pages.length - 1) {
-        currentPage++;
-        showPage(currentPage);
-      }
-    })
-
-    // show first page initially
+// prev button
+$(".prev-btn").on("click touchstart", function(e){
+  e.preventDefault(); // avoid double trigger
+  if (currentPage > 0) {
+    currentPage--;
     showPage(currentPage);
+  }
+});
+
+// next button
+$(".next-btn").on("click touchstart", function(e){
+  e.preventDefault();
+  if (currentPage < pages.length - 1) {
+    currentPage++;
+    showPage(currentPage);
+  }
+});
+
+// show first page initially
+showPage(currentPage);
